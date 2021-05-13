58cd7ef6df479c4d408742c85974d3e435a1adb5
ericasecrieru <75315735+ericasecrieru@users.noreply.github.com>
2021-05-13 16:01:59 +0300

    Initial commit

---
+++ cypress/pageElements/elements.js
@@ -0,0 +1,4 @@
+export const username = '[placeholder="Username"]'
+export const password = '[placeholder="Password"]'
+export const submitbtn = '[class="submit-button btn_action"]'
+export const title = '[class="title"]'
