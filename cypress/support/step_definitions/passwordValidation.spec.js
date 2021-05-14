454955c5f76c68dee53c9346eeab58aa75c3a581
ericasecrieru <75315735+ericasecrieru@users.noreply.github.com>
2021-05-14 10:39:50 +0300

    added passwordValidation spec

---
+++ cypress/support/step_definitions/passwordValidation.spec.js
@@ -0,0 +1,24 @@
+
+/// <reference types="Cypress" />
+import Credentials from '../../fixtures/Credentials.json'
+import * as pageElems from '../../pageElements/elements.js'
+
+
+Given('I am on login page', function () {
+    cy.visit(Cypress.env('login_url'))
+})
+
+When('I fill username and password fields {string} {string}', function (username, password) {
+    cy.get(pageElems.username).type(username)
+	cy.get(pageElems.password).type(password)
+})
+ 
+And('click on Login button', function () {
+    cy.get(pageElems.submitbtn).click() 
+})
+
+
+Then('A Epic sadface: Username and password do not match any user in this service message is displayed', function () {
+    cy.get(pageElems.message).should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+})
+
